34919b416b4f5466092527cdd028aa4bf1b746bd
Owen Jones <owenthefirst93@gmail.com>
2020-11-25 17:00:21 -0500

    modal comp and dashboard

---
+++ src/Components/Modal.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+
+const Modal = () => {
+    return(
+        <div></div>
+    )
+}
---
+++ src/Components/ProjectDashboard.jsx
@@ -0,0 +1,24 @@
+import React,{Component} from 'react'
+
+
+class ProjectDashboard extends Component {
+    constructor(){
+        super()
+        this.state = {
+            modalState : false
+        }
+    }
+
+    render(){
+        const {modalState} = this.state
+        return(
+            <div class='dashboard'>
+                
+            </div>
+        )
+    }
+}
+
+
+
+export default ProjectDashboard
